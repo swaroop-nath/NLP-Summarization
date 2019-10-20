626b75776ed4e2e5b0561c6a7db08b5e59d09d70
swaroop-nath <swaroopnath6@gmail.com>
2019-10-20 23:30:40 +0530

    Basic TFIDF approached summarizer built, considering sentences as documents.

--- TFIDF Summarizer/basic_tfidf_summarizer.py
+++ TFIDF Summarizer/basic_tfidf_summarizer.py
@@ -3,6 +3,7 @@ import re
 from nltk.tokenize import sent_tokenize, word_tokenize
 import string
 from nltk.corpus import stopwords
+from numpy import log
 
 # Importing the dataset
 raw_data = ''
@@ -27,9 +28,9 @@ words = [word for sent in sentences for word in word_tokenize(sent)]
 stop_words = stopwords.words('english')
 words = [word for word in words if word not in stop_words]
 
+# Calculating the frequency of words
 term_freq_matrix = {}
 
-# Calculating the frequency of words
 for word in words:
     if term_freq_matrix.get(word) == None:
         term_freq_matrix[word] = 1
@@ -39,6 +40,27 @@ for word in words:
 count_words = len(term_freq_matrix)
 for key, value in term_freq_matrix.items():
     term_freq_matrix[key] = value/count_words
+    
+# Calculating Inverse Document Frequency - treating each sentence as a document
+num_docs = len(original_sentences)
+
+idf_matrix = {}
+
+for word in words:
+    if idf_matrix.get(word) == None:
+        count = 0
+        for sent in sentences:
+            if word in sent: count += 1
+        idf_matrix[word] = count
+
+for key in idf_matrix.keys():
+    idf_matrix[key] = log(num_docs/idf_matrix[key])
+   
+# Calculating the TFIDF score for each word
+tf_idf_score = {}
+
+for key in term_freq_matrix.keys():
+    tf_idf_score[key] = term_freq_matrix[key] * idf_matrix[key]
     
 # Scoring sentences
 sentence_scores = {}
@@ -50,7 +72,7 @@ for i, sent in enumerate(sentences):
     sent_words = word_tokenize(sent)
     for word in sent_words:
         if word in words:
-            sentence_scores[original_sentences[i]] += term_freq_matrix[word]
+            sentence_scores[original_sentences[i]] += tf_idf_score[word]
             
 num_output_sentences = 4
 threshold = sorted(list(sentence_scores.values()), reverse = True)[num_output_sentences]
